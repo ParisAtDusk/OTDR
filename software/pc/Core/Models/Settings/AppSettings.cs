9e29a285aa690d1494eea79af99647b9ab8b6417
ParisAtDusk <paryzk@interia.pl>
2026-07-11 22:37:48 +0200

    Changed default plotting backend

--- software/pc/Core/Models/Settings/AppSettings.cs
+++ software/pc/Core/Models/Settings/AppSettings.cs
@@ -10,7 +10,7 @@ public sealed class AppSettings
     };
     public enum PlottingBackend { LiveCharts, ScottPlot }
     public Theme CurrentTheme { get; set; } = Theme.Default;
-    public PlottingBackend PlottingLibrary { get; set; } = PlottingBackend.LiveCharts;
+    public PlottingBackend PlottingLibrary { get; set; } = PlottingBackend.ScottPlot;
     public double WindowWidth { get; set; } = 1200;
     public double WindowHeight { get; set; } = 800;
 }
