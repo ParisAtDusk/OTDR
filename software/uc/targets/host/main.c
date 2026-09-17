a754771dcd87799cb88608ea5f042cfc03930b6c
ParisAtDusk <paryzk@interia.pl>
2026-09-17 14:43:57 +0200

    [wip] FreeRTOS

--- software/uc/targets/host/main.c
+++ software/uc/targets/host/main.c
@@ -1,8 +1,19 @@
+#include "FreeRTOS.h"
 #include "scpi_commands_otdr.h"
+#include "task.h"
 #include "transport_if.h"
 #include "transport_tcp.h"
 #include <stdint.h>
 #include <stdio.h>
+
+static void test_task(void *arg) {
+  while (1) {
+    printf("Hello from FreeRTOS\n");
+    fflush(stdout);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
 
 transport_t transport;
 
@@ -13,5 +24,9 @@ int main(void) {
   char data[] = "test data\n\r";
   transport_send(&transport, (uint8_t *)&data, sizeof(data));
   printf("Hello = %d\n\r", ui);
+
+  xTaskCreate(test_task, "Test", 1024, NULL, 1, NULL);
+
+  vTaskStartScheduler();
   return 0;
 }
